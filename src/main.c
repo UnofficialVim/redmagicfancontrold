13f5073776f025d572e7bd8240fd62341455ac09
UnofficialVim <willemtoews@gmail.com>
2026-07-03 13:49:57 -0600

    Refactor CMakeLists.txt for improved source organization and add platform-specific configurations; remove debug log level setting in main.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 
 int main(void) {
 	log_init("redmagicfancontrold.log");
-	set_log_level(LOG_DEBUG);
 	log_write(LOG_INFO, "RedMagic Fan Control Daemon started");
 
 	//start lifecycle of the daemon here
